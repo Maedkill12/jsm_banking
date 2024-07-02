37db27d42b02b53d8dbb2e3d5f5d84737beed63f
MaedKill12 <maed012014@gmail.com>
2024-07-02 14:07:14 -0600

    auth ui

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    // Mutation / Database / Make fetch
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // Create user account
+  } catch (error) {
+    console.error(error);
+  }
+};
